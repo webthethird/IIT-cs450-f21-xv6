ca65059e1d3fda6f36d36694a6ee66ecd08f48c3
Teleportal <joshthegreenberg@gmail.com>
2021-11-06 15:33:37 -0500

    finished syscall, some virtual memory function changes for shared pages

--- defs.h
+++ defs.h
@@ -185,6 +185,9 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
+void            initshpgs(void);
+int             getshpg(int, int);
+int             freeshpg(int);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
