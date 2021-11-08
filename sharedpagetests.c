d9ebf5700104ee697b901f7a6c92cf2575a16920
webthethird <ratzbodell@gmail.com>
2021-11-07 19:52:58 -0600

    Add tests after freeing shared pages (page faults)

--- sharedpagetests.c
+++ sharedpagetests.c
@@ -7,13 +7,25 @@ int main(int argc, char const *argv[])
   //test if a process can call getSharedPages and then free them and exit
   // char *page = getshpg(0, 1);
   // freeshpg(0);
-  //test if a process can call getSharedPages, write to those pages, that work is readable, and then free them
-  
+
+  //test if a process can call getSharedPages, write to those pages, and read from the pages, 
+  //then after calling freeSharedPages, it can no longer read from them
+  /*
+  char *page = getshpg(0, 1);
+  char *str1 = strcpy(page, "Testing testing\n");
+  printf(1, "%s", str1);
+  char *str2 = strcpy(page, "1, 2, 3\n");
+  printf(1, "%s", str2);
+  freeshpg(0);
+  char *str3 = strcpy(page, "boom\n");
+  printf(1, "After freeshpg: %s\n", str1);
+  */
+
   //test if a process can call getSharedPages, then call getSharedPages with a different key, do different work on each
   //and then free those pages
 
   //test if a two processes can call getsharedpages, one can write, and the other can read
-  /*
+  
   int ret;
   char *page;
   if((ret = fork()) == 0) {
@@ -26,8 +38,14 @@ int main(int argc, char const *argv[])
     printf(1, "Parent reading: %s\n", page);
   }
   freeshpg(0);
-  */
+  // also test whether child process can write to page after freeing (should cause page fault)
+  // if(ret == 0) {
+  //   printf(1, "Child reading after freeing: %s\n", page);
+  //   strcpy(page, "(this shouldn't be here)");
+  // }
+
   //test that two processes can call getsharedpages with different keys and get different pages
+  /*
   int ret, key;
   char *page;
   if((ret = fork()) == 0) {
@@ -42,6 +60,6 @@ int main(int argc, char const *argv[])
     printf(1, "Parent reading: %s\n", page);
   }
   freeshpg(key);
-
+  */
   exit();
 }
