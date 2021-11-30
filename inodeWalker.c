fc458c0fdb88994278ebe68ff9c311ba56605398
Teleportal <joshthegreenberg@gmail.com>
2021-11-30 11:18:47 -0600

    added inode table walker test

---
+++ inodeWalker.c
@@ -0,0 +1,23 @@
+/*
+test program by:
+William Bodell - CWID: A20465701
+Josh Greenberg - CWID: A20472596
+*/
+#include "types.h"
+#include "user.h"
+#include "fs.h"
+
+
+int main(int argc, char const *argv[])
+{
+    int inodes[200];
+    mkdir("/test");
+    chdir("/test");
+    intbwalk(1, inodes);
+    int i;
+    while (inodes[i] != 0) {
+        printf("Inode %d is allocated.\n", inodes[i]);
+        i++;
+    }
+    exit();
+}
